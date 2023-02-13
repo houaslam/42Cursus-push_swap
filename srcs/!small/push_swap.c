64ef38eeee534577ff621d572081386c7252a718
Hajar Ouaslam <houaslam@e2r9p9.1337.ma>
2023-02-13 19:55:19 +0100

    10

--- srcs/!small/push_swap.c
+++ srcs/!small/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: houaslam <houaslam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 19:30:01 by houaslam          #+#    #+#             */
-/*   Updated: 2023/02/13 13:54:56 by houaslam         ###   ########.fr       */
+/*   Updated: 2023/02/13 19:52:11 by houaslam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	push_swap(t_data *data, t_list **stack_a, t_list **stack_b)
 	t_list	*t;
 
 	tmp = *stack_a;
-	while (data->size_a&& check_index(stack_a, data->n) == 1)
+	while (data->size_a != 0 && check_index(stack_a, data->n) == 1)
 	{
 		if (tmp->index <= data->n)
 		{
@@ -27,29 +27,15 @@ void	push_swap(t_data *data, t_list **stack_a, t_list **stack_b)
 			data->size_b++;
 			tmp = *stack_a;
 		}
-		// else if (check_index(stack_a, data->n) == 0)
-		// {
-		// 	get_back(stack_a);
-		// 	get_val_end(stack_a, data);
-		// 	data->n = data->size_a / 2;
-		// }
-		else if (tmp->index >= data->n)
+		else if (tmp->index > data->n)
 		{
 			t = ft_lstlast(*stack_a);
 			if (t->index <= data->n)
-			{
 				write_rr(stack_a, "rra\n");
-				break ;
-			}
-			write_r(stack_a, "ra\n");
+			else
+				write_r(stack_a, "ra\n");
 			tmp = *stack_a;
 		}
-		// if (data->size_a == 3)
-		// {
-		// 	check_do(stack_a);
-		// 	break ;
-		// }
-		// aff(*stack_a);
 	}
 }
 
@@ -58,9 +44,9 @@ void	figure_para(t_data *data)
 	if (data->size_a <= 10)
 		data->n = 2;
 	else if (data->size_a <= 150)
-		data->n = 8;
+		data->n = 4;
 	else if (data->size_a <= 500)
-		data->n = 40;
+		data->n = 8;
 	data->n = data->size_a / data->n * data->factor;
 }
 
@@ -71,7 +57,6 @@ void	push_back(t_data *data, t_list **stack_b, t_list **stack_a)
 	tmp = *stack_b;
 	while (tmp)
 	{
-		// printf("%d\n", data->size_b);
 		if (tmp->index == data->size_b)
 		{
 			printf("found\n");
